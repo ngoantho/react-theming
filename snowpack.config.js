4fe9d55eb1af4ecc6ea3db8c01ceaa27b0f2c223
Anthony Ngo <anthonyngo002@gmail.com>
2020-12-20 00:32:45 -0800

    initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,27 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: {url: '/', static: true},
+    src: {url: '/dist'},
+  },
+  plugins: ['@snowpack/plugin-dotenv', '@prefresh/snowpack'],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+
+  alias: {
+    /* ... */
+  },
+};
